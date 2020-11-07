349d43567ebb60f72ee12f806ae46b3cd3b5f656
nahyung <nhj12311@gmail.com>
2020-11-07 20:59:07 +0900

    end work search blog.

--- search_blog.js
+++ search_blog.js
@@ -19,12 +19,9 @@ let options = {
     json:true 
 };
 
-(async () => {
-
-    let pageIdx = 1;
-
+getCopyList = async () => {
     let copyList =[];
-
+    let pageIdx = 1;
     while(true){
         options.uri = `https://ameagaru3241.tistory.com/?page=${pageIdx++}`;    
         const result = await request(options);
@@ -35,17 +32,62 @@ let options = {
         }
         list_li.each(
             (idx, el)=>{
-                copyList.push({ href : $(el).find("a").attr("href"), title : $(el).find("a").text().trim()  });
-                console.log(idx, $(el).find("a").attr("href"), $(el).find("a").text());
+                copyList.push({ href : $(el).find("a").attr("href"), title : $(el).find("a").text().replace("출처: https://nhj12311.tistory.com/397 [This is IT]","").trim()  });
+                //console.log(idx, $(el).find("a").attr("href"), $(el).find("a").text());
             }
         )
-        console.log("wait 1sec");
-        await sleep(1000);
+        console.log("wait 1sec... index : ", pageIdx);
+        await sleep(500);
     }
+    return copyList;
+}
 
+getMyList = async () => {
+    let copyList =[];
+    let pageIdx = 1;
+    while(true){
+        options.uri = `https://nhj12311.tistory.com/?page=${pageIdx++}`;    
+        const result = await request(options);
+        const $ = cheerio.load(result);
+        const list_li = $("#mArticle");
+        if( list_li && list_li.length == 0 ){
+            break;
+        }
+        list_li.each(
+            (idx, el)=>{
+                copyList.push({ href : $(el).find("a").attr("href"), title : $(el).find(".tit_post").text().replace("출처: https://nhj12311.tistory.com/397 [This is IT]","").trim()  });
+                //console.log(idx, $(el).find("a").attr("href"), $(el).find("a").text());
+            }
+        )
+        console.log("wait 1sec... index : ", pageIdx);
+        await sleep(500);
+    }
+    return copyList;
+}
+
+(async () => {
+    const copyList = await getCopyList();
+    const myList = await getMyList();
+    const dupList = [];
+    
+    copyList.forEach(
+        (el, idx ) => {
+            myList.forEach(
+                (el2, idx2 ) => {
+                    //console.log(idx, "  ", el.title, "    " , el.href, "  " ,el2.href);
+                    if( el.title == el2.title ){
+                        console.log(idx, "  ", el.title, "    원본URL : https://nhj12311.tistory.com" + el2.href, "  신고URL : https://ameagaru3241.tistory.com" + el.href);
+                        console.log("");
+                    }
+                }
+            )
+        }
+    );
     
     
 
+
+    //console.table(myList);
 })();
 
 
---
+++ test.js
@@ -0,0 +1,5 @@
+var list = [{a:"1"},{a:"2"}];
+
+list.forEach((idx, el)=>{
+    console.log(idx, el);
+});
